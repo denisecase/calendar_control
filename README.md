b0a520351c4342b3b1e063b80eca3556518ef164
Denise Case <dcase@nwmissouri.edu>
2017-04-16 12:00:11 -0500

    update to include ind events from academic calendar

--- README.md
+++ README.md
@@ -2,27 +2,52 @@
 
 Add week events to Google Calendar at the start of a new semester (from the command line). 
 
+## Setup
+
+Install Python 2.6 or greater.  See instructions for setting up environment variables and path.
+
+Install pip for Python package management.
+
+Follow the instructions to set up access to your Google calendar from your local Python app.
+
+Set the options:
+
+* showcolors = False
+* addRepeating = False
+* addIndividual = True
+
+Set associated variables (see file).
+
+When pulling dates, some times may have invalid characters - clean up csv data files before running. 
+
+
 ## Resources
  
-* [Git for Windows](https://msysgit.github.io/) - provides a Git Bash window from Windows Explorer.
+* [Git for Windows](https://msysgit.github.io/) 
 * [Python](https://www.python.org/downloads/)
 * [Google Developers Google Calendar API Python Quickstart](https://developers.google.com/google-apps/calendar/quickstart/python)
 * [What does if main do?](http://stackoverflow.com/questions/419163/what-does-if-name-main-do)
 * [List of time zone options](http://stackoverflow.com/questions/22526635/list-of-acceptable-google-calendar-api-time-zones)
-*  [How to delete stored credentials](http://stackoverflow.com/questions/30293293/insufficient-permissions-on-google-calendar-apis-acl-list/30675031#30675031) 
+*  [How to delete stored credentials](http://stackoverflow.com/questions/30293293/insufficient-permissions-on-google-calendar-apis-acl-list/30675031#30675031)
+* [Northwest Missouri State University Academic Calendar](http://www.nwmissouri.edu/academics/calendar.htm)
 
 ## Getting started (on Windows)
 
-Before working through the Python quickstart guide, install Git for Windows.
-During installation, check the "Git Bash Here" option.  
-Then, from Windows Explorer, you can open a Git Bash window by right-clicking on a folder
-and execute Linux commands in that location. 
+Clone the repo.
 
-Edit the semester start date on line 28.
-Edit the number of weeks in the semester on line 29.
+```
+pip install httplib2
+pip install apiclient
+pip install --upgrade google-api-python-client
+```
 
-Open a command window as administrator in the calendar_control folder, and type:
-> py calendar_control.py
+Edit the semester start year, month, day.
+Edit the number of weeks in the semester.
+
+
+```
+python calendar_control.py
+```
 
 
 
--- calendar_control.py
+++ calendar_control.py
@@ -7,6 +7,7 @@ from oauth2client import client
 from oauth2client import tools
 
 import datetime
+import csv
 
 try:
     import argparse
@@ -24,11 +25,25 @@ APPLICATION_NAME = 'Google Calendar API Quickstart'
 # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
 # stored credentials.
 """
+# enter semester start as integers
+startyear = 2019
+startmonth = 1
+startday = 14
 
-sem_start =  datetime.date(2016, 1, 11)
-wks_in_sem = 15
-color_id = 10
+wks_in_sem = 16
+color_id = 8
+
+email_invite = "dcase@nwmissouri.edu"
+name_invite = "Denise Case (NWMSU)"
+
+csvfile = 'one.csv'
+
+showcolors = False
+addRepeating = False
+addIndividual = True
+
                                     
+sem_start =  datetime.date(startyear, startmonth, startday)                        
 
 def get_credentials():
     """
@@ -85,58 +100,106 @@ def main():
         print ( start + "   " + event['summary'] )
         
     colors = service.colors().get().execute()
-
-    """
-    # Print available calendarListEntry colors for reference.
-    for id, color in colors['calendar'].items(): #python 3
-      print ('colorId: %s' % id)
-      print ('  Background: %s' % color['background'])
-      print ('  Foreground: %s' % color['foreground'])
-      
-    # Print available event colors for reference.
-    for id, color in colors['event'].items():
-      print ('colorId: %s' % id)
-      print ('  Background: %s' % color['background'])
-      print ('  Foreground: %s' % color['foreground'])  
-    """
+   
+    if showcolors:
+        # Print available calendarListEntry colors for reference.
+        for id, color in colors['calendar'].items(): #python 3
+            print ('colorId: %s' % id)
+            print ('  Background: %s' % color['background'])
+            print ('  Foreground: %s' % color['foreground'])
+            
+        # Print available event colors for reference.
+        for id, color in colors['event'].items():
+            print ('colorId: %s' % id)
+            print ('  Background: %s' % color['background'])
+            print ('  Foreground: %s' % color['foreground'])    
     
-    """
-    # add recurring events	
-    """
-    for i in range(1, wks_in_sem + 1):
-    #for i in range(14,17):
-        weekstr = 'Week ' + str(i)
-        print (weekstr)
-        
-        mon = sem_start + datetime.timedelta(weeks=i-1)
-        print ('mon =' + str(mon))
-        
-        monstr = mon.strftime("%Y-%m-%d").lstrip("0")
-        print ('monstr =' + monstr)
-        
-        rec_event = {
-          'summary': weekstr,
-          'location': 'NWMissouri State',
-          'start': {
-          'date': monstr,
-          'timeZone': 'America/Chicago'
-          },
-          'end': {
-          'date': monstr,
-          'timeZone': 'America/Chicago'
-          },
-          'recurrence': [
-          'RRULE:FREQ=DAILY;COUNT=5',
-          ],
-          'reminders': {
-          'useDefault': False,
-          'overrides': [],
-          },
-          'colorId': str(color_id),
-		}
+    if addRepeating:
+        # add recurring events	
+        for i in range(1, wks_in_sem+1):
+        #for i in range(wks_in_sem, wks_in_sem+1):
+            weekstr = 'Week ' + str(i)
+            print (weekstr)
+            
+            mon = sem_start + datetime.timedelta(weeks=i-1)
+            print ('mon =' + str(mon))
+            
+            monstr = mon.strftime("%Y-%m-%d").lstrip("0")
+            print ('monstr =' + monstr)
+            
+            rec_event = {
+            'summary': weekstr,
+            'location': 'NWMissouri State',
+            'start': {
+            'date': monstr,
+            'timeZone': 'America/Chicago'
+            },
+            'end': {
+            'date': monstr,
+            'timeZone': 'America/Chicago'
+            },
+            'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=5',
+            ],
+            'reminders': {
+            'useDefault': False,
+            'overrides': [],
+            },
+            "attendees": [
+                {
+                "email": email_invite,
+                "displayName": name_invite
+                }
+            ],
+            'colorId': str(color_id),
+            }
 
-        recurring_event  = service.events().insert(calendarId='primary', body=rec_event).execute()
-        print (recurring_event ['id'])
+            recurring_event  = service.events().insert(calendarId='primary', body=rec_event).execute()
+            print (recurring_event ['id'])
+    
+    if addIndividual:
+        # add individual events from csv file
+        f = open(csvfile, 'r')
+        print f
+
+        for line in csv.reader(f):
+            print line
+
+            title, y, m, d = line
+            print (title)
+            
+            mon = datetime.date(int(y), int(m), int(d))  
+            print ('mon =' + str(mon))
+            
+            monstr = mon.strftime("%Y-%m-%d").lstrip("0")
+            print ('monstr =' + monstr)
+            
+            rec_event = {
+            'summary': title,
+            'location': 'NWMissouri State',
+            'start': {
+            'date': monstr,
+            'timeZone': 'America/Chicago'
+            },
+            'end': {
+            'date': monstr,
+            'timeZone': 'America/Chicago'
+            },
+            'reminders': {
+            'useDefault': False,
+            'overrides': [],
+            },
+            "attendees": [
+                {
+                "email": "dcase@nwmissouri.edu",
+                "displayName": "Denise Case (NWMSU)"
+                }
+            ],
+            'colorId': str(color_id),
+            }
+
+            ind_event  = service.events().insert(calendarId='primary', body=rec_event).execute()
+            print (ind_event ['id'])
 
 if __name__ == '__main__':
     main()
